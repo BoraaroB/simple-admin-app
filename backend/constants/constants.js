f27e907d072a0b4673325e8bb2a7b2ff23bbfa83
BoraaroB <borislavstamenic@gmail.com>
2020-04-06 20:05:49 +0200

    Init commit

---
+++ backend/constants/constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+  USER: {
+    ROLES: {
+      ADMIN: 1,
+      MANAGER: 2,
+      USER: 3
+    },
+    ALL_ROLES: [1, 2, 3]
+  },
+  INIT_ADMIN: {
+    ADMIN_EMAIL: 'admin@admin.com',
+    ADMIN_PASSWORD: 'password',
+    ADMIN_ROLE: 1
+  }
+}
